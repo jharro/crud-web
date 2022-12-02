0488ee443da07e2502fa6f3bc4efd40b33f72304
jharro <jharro@gmail.com>
2022-12-02 10:17:08 -0500

    Proyecto Java spring Boot para la ficha 2559206

--- src/main/java/com/example/crudweb/modelo/Productos.java
+++ src/main/java/com/example/crudweb/modelo/Productos.java
@@ -1,2 +1,18 @@
-package com.example.crudweb.modelo;public class Productos {
+package com.example.crudweb.modelo;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name="productos")
+@Data
+public class Productos {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nombre;
+    private String descripcion;
+    private double precio;
+
+
 }
