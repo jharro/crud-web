8a2d92b621d74042ba53ecf8f941fb612fd285fb
jharro <jharro@gmail.com>
2022-12-02 10:17:08 -0500

    Proyecto Java spring Boot para la ficha 2559206

--- src/main/java/com/example/crudweb/controlador/ProductoControlador.java
+++ src/main/java/com/example/crudweb/controlador/ProductoControlador.java
@@ -1,2 +1,31 @@
-package com.example.crudweb.controlador;public class ProductoControlador {
+package com.example.crudweb.controlador;
+
+import com.example.crudweb.respositorio.ProductoRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProductoControlador {
+
+    @Autowired
+    private ProductoRepositorio productoRepositorio;
+
+    @GetMapping("")
+    public String home(){
+        return "index";
+    }//http://localhost:8080/
+
+    @GetMapping("/productos")//http://localhost:8080/productos
+    public String productos(Model model){
+        model.addAttribute("productos",productoRepositorio.findAll());
+     //   model.addAttribute("productos",productoRepositorio.findAll());
+        return "productos";
+    }
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
 }
--- src/main/java/com/example/crudweb/respositorio/ProductoRepositorio.java
+++ src/main/java/com/example/crudweb/respositorio/ProductoRepositorio.java
@@ -1,2 +1,9 @@
-package com.example.crudweb.respositorio;public interface ProductoRepositorio {
+package com.example.crudweb.respositorio;
+
+import com.example.crudweb.modelo.Productos;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductoRepositorio extends JpaRepository<Productos,Integer> {
 }
